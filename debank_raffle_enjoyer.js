784c43288fff18b3c093228d926c3df0a9f24714
Jokerank <nikitalarkov1997@yandex.ru>
2023-07-26 06:25:56 +0700

    changing version because of fix

--- debank_raffle_enjoyer.js
+++ debank_raffle_enjoyer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         debank_raffle_enjoyer
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.4.2
 // @description  try to take over the world!
 // @author       Jokerank
 // @match        *://*debank.com/*
